12623160e80e0f949d80e7cff85449c27afa95be
Zhang Tianyu <ztyawc@gmail.com>
2024-07-24 13:51:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -24,7 +24,7 @@ let addressesnotls = [
 
 // 设置优选noTLS地址api接口
 let addressesnotlsapi = [
-	'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
+	//'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 ];
 
 let DLS = 8;//速度下限
