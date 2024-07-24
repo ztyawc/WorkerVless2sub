eea0bfa113ec3d6d461761847e21d7ed2e8d75c9
Zhang Tianyu <ztyawc@gmail.com>
2024-07-24 22:16:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -27,9 +27,9 @@ let addressesnotlsapi = [
 	//'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 ];
 
-let DLS = 8;//速度下限
+let DLS = 5;//速度下限
 let addressescsv = [
-	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressescsv.csv', //iptest测速结果文件。
+	'https://raw.githubusercontent.com/ztyawc/csv/main/result.csv', //iptest测速结果文件。
 ];
 
 let subconverter = "subapi-loadbalancing.pages.dev"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
