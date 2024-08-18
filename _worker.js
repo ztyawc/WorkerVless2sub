aefe84087a4a5d4a40e1cd2fae59dc21a02e92ab
Zhang Tianyu <ztyawc@gmail.com>
2024-08-18 17:24:49 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -27,7 +27,7 @@ let addressesnotlsapi = [
 	//'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 ];
 
-let DLS = 6;//速度下限
+let DLS = 5;//速度下限
 let addressescsv = [
 	'https://raw.githubusercontent.com/ztyawc/csv/main/result.csv', //iptest测速结果文件。
 ];
